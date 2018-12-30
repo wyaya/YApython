d5b6490690e671e7a35f7a20f53752203bbdad5e
YAYA <wyaya776@163.com>
2018-12-30 14:27:30 +0800

    add blog project

---
+++ blogproject/blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+#from django.shortcuts import HttpResponse
+from . models import Post
+
+def index(request):
+    # return render(request, 'blog/index.html', context={
+    #     'title': '我的博客首页',
+    #     'welcome': '欢迎访问我的博客首页'
+    # })
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request,'blog/index.html',{'post_list': post_list})
+
+def detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blog/detail.html',{'post':post})
